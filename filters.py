c23766c41b8294ae220f684681e7d79f01fa457d
DENLID <denlid1507@gmail.com>
2024-02-26 18:04:38 +0200

    some change

--- filters.py
+++ filters.py
@@ -1,10 +1,14 @@
 from typing import List
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
-
+from pymongo import MongoClient
 import config
+
+cluster = MongoClient(config.mongo_api)
+users = cluster.ILdb.users
+ban_list = cluster.ILdb.ban_list
+
 
 class IsAdmin(BaseFilter):
     async def __call__(self, message: Message) -> bool:
-        if isinstance(config.admins, int):
-            return config.admins == message.chat.id
+        return message.chat.id in config.admins
--- handlers.py
+++ handlers.py
@@ -10,6 +10,7 @@ import json
 from register import register
 from update_info import update_info_ms
 from states import Communication
+from filters import IsAdmin
 import keyboards
 import config
 
@@ -157,35 +158,36 @@ async def wad_handler(message: Message):
 async def wad_handler(message: Message):
     await wad_message(message)
 
-@router.message(Communication.mess)
+@router.message(Communication.mess, not IsAdmin)
 async def handle_text(message: Message):
-    if message.chat.id != config.admin_group:
-        if ban_list.find_one({"_id": message.chat.id}) == None:
-            text = f"""
+    if ban_list.find_one({"_id": message.chat.id}) == None:
+        text = f"""
 {message.text}
 
 ID: {message.chat.id} | USERNAME: {message.from_user.username} |
 """
-            if message.content_type == "text":
-                await bot.send_message(chat_id=config.admin_group, text=text)
-            if message.content_type == "photo":
-                await bot.send_photo(chat_id=config.admin_group, photo=message.photo[-1].file_id, caption=text)
-            if message.content_type == "video":
-                await bot.send_video(chat_id=config.admin_group, video=message.video.file_id, caption=text)
-            if message.content_type == "document":
-                await bot.send_document(chat_id=config.admin_group, document=message.document.file_id, caption=text)
-        else:
-            await message.answer("""
+        if message.content_type == "text":
+            await bot.send_message(chat_id=config.admin_group, text=text)
+        if message.content_type == "photo":
+            await bot.send_photo(chat_id=config.admin_group, photo=message.photo[-1].file_id, caption=text)
+        if message.content_type == "video":
+            await bot.send_video(chat_id=config.admin_group, video=message.video.file_id, caption=text)
+        if message.content_type == "document":
+            await bot.send_document(chat_id=config.admin_group, document=message.document.file_id, caption=text)
+    else:
+        await message.answer("""
 Ви отривали бан, тому не можете 
 надсилати повідомлення адміністраторам.
 """)
         
 
-@router.message()
+@router.message(IsAdmin)
 async def handle_text(message: Message):
-    if message.chat.id == config.admin_group:
+    try:
         text = message.reply_to_message.text
         start_index = text.find("ID: ") + len("ID: ")
         end_index = text.find(" | USERNAME:")
         id = text[start_index:end_index]
         await bot.copy_message(id, config.admin_group, message.message_id)
+    except:
+        pass
