d40bc5aa98ca53f49500cf9d9271836e554d7286
DENLID <denlid1507@gmail.com>
2024-03-12 08:45:07 +0200

    some fix

--- handlers/air_alert.py
+++ handlers/air_alert.py
@@ -19,7 +19,7 @@ async def airalert_handler(message: Message, db: MDB):
         weekday = dt.weekday()
         
         if weekday != 5 and weekday != 6 and datetime(dt.year, dt.month, dt.day, 8, 0, tzinfo=ukraine_time) <= dt <= datetime(dt.year, dt.month, dt.day, 18, 40, tzinfo=ukraine_time):
-            for u in await db.users.find({ "$or": [{"airalert": "st"}, {"airalert": "always"}]}):
+            async for u in await db.users.find({ "$or": [{"airalert": "st"}, {"airalert": "always"}]}):
                 await message.copy_to(u["_id"]) # Надсилання користувачам
             await message.copy_to(config.channel_to) # Надсилання на канал
         else:
